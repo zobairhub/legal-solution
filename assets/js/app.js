7985a9277a95247f62e579beec2e168e6e22170a
zobairgithub <darazbusinessinfo@gmail.com>
2023-07-25 01:32:02 +0600

    legal solution website created

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'theme-btn-bg': '#B68C5A',
+        'menu-text-color': '#707070',
+      }
+    }
+  }
+}
